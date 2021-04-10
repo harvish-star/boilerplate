6ecbfd1a5b0445f2a3aaf2f697c1d47b1ac3b433
harvish-star <82267372+harvish-star@users.noreply.github.com>
2021-04-10 16:56:25 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# boilerplate
-classic game
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400,400);
+  //createSprite(400, 400, 100, 100);
+}
+
+function draw() {
+  background(200); 
+  rectMode(CENTER);
+  rect(200,200,100,100);
+  drawSprites();
+}
